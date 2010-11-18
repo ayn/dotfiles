31f6009abf61ecd3bf864935aae855ce427c8ba1
ayn <ayn@andrewng.com>
2010-11-18 13:38:52 -0800

    adding my custom zsh file for ohmyzsh

---
+++ zsh/ayn.zsh
@@ -0,0 +1,10 @@
+alias ec2='ssh andrewng.com'
+alias gf='git diff'
+alias lal='ls -alFh'
+alias lt='ls -ltrh'
+alias pass='passenger start -a 127.0.0.1 -p 3000 -d'
+alias rst='touch tmp/restart.txt'
+alias src='source ~/.zshrc'
+alias tlog='tail -f log/development.log'
+grepall(){ find . -type f -print0 | xargs -0 grep -n "$@" }
+export EDITOR='vim'
