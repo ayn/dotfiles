4015b056e0aa0e77fe31e2de0c123954f836924f
ayn <ayn@andrewng.com>
2010-11-19 15:44:50 -0800

    alias to sync addressbook to google

--- zsh/ayn.zsh
+++ zsh/ayn.zsh
@@ -8,6 +8,7 @@ alias src='source ~/.zshrc'
 alias tlog='tail -f log/development.log'
 alias sc3='rails console'
 alias srd='screen -r -d'
+alias synccontacts='/System/Library/PrivateFrameworks/GoogleContactSync.framework/Versions/A/Resources/gconsync --sync com.google.ContactSync'
 grepall(){ find . -type f -print0 | xargs -0 grep -n "$@" }
 c(){ cd ~/work/$1 }
 export EDITOR='vim'
