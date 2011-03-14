72dfd3c42d687d6a80766783b8bcf71c68cad270
ayn <ayn@andrewng.com>
2011-03-14 15:06:15 -0700

    bundle open to use Textmate

--- zsh/ayn.zsh
+++ zsh/ayn.zsh
@@ -10,6 +10,7 @@ alias src='source ~/.zshrc'
 alias srd='screen -r -d'
 alias synccontacts='/System/Library/PrivateFrameworks/GoogleContactSync.framework/Versions/A/Resources/gconsync --sync com.google.ContactSync'
 alias tlog='tail -f log/development.log'
+bo(){ EDITOR=mate bundle open $1 }
 grepall(){ find . -type f -print0 | xargs -0 grep -n "$@" }
 c(){ cd ~/work/$1 }
 export EDITOR='vim'
