c7105a9a86f80bd6dd63f37c391f5b198eedce88
ayn <ayn@andrewng.com>
2011-06-22 18:53:13 -0700

    using ^N and ^P for vi insert mode search

--- zsh/ayn.zsh
+++ zsh/ayn.zsh
@@ -20,5 +20,5 @@ c(){ cd ~/work/$1 }
 bindkey -M vicmd '?' history-incremental-pattern-search-backward
 bindkey -M vicmd '/' history-incremental-pattern-search-forward
 # set up for insert mode too
-bindkey -M viins '^R' history-incremental-pattern-search-backward
-bindkey -M viins '^F' history-incremental-pattern-search-forward
+bindkey -M viins '^P' history-incremental-pattern-search-backward
+bindkey -M viins '^N' history-incremental-pattern-search-forward
