257a7b852ac8169439decc35990a589401bbedfe
Andrew Ng <ayn@andrewng.com>
2011-12-28 18:14:25 -0800

    more janus setup

--- zsh/ayn.zsh
+++ zsh/ayn.zsh
@@ -23,3 +23,20 @@ bindkey -M vicmd '/' history-incremental-pattern-search-forward
 # set up for insert mode too
 bindkey -M viins '^P' history-incremental-pattern-search-backward
 bindkey -M viins '^N' history-incremental-pattern-search-forward
+
+function define_vim_wrappers()
+{
+  vim_commands=(
+    eview evim gview gvim gvimdiff gvimtutor rgview
+    rgvim rview rvim vim vimdiff vimtutor xxd mvim
+  )
+
+  for cmd in ${vim_commands[@]}; do
+    cmd_path=`/usr/bin/env which -a "${cmd}" 2>/dev/null | grep '^/'`
+    if [ -x "${cmd_path}" ]; then
+      eval "function ${cmd} () { (unset GEM_HOME; unset GEM_PATH; $cmd_path \$@) }"
+    fi
+  done
+}
+
+define_vim_wrappers
